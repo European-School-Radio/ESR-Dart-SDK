1744fc04765a1d22671abf0f467f961b51a7f258
George Papadopoulos <georgepap2001@gmail.com>
2024-12-30 18:07:31 +0200

    Fixed web base url on server config class.

--- lib/src/global_parameters/server_config.dart
+++ lib/src/global_parameters/server_config.dart
@@ -3,5 +3,5 @@ class ESRServerConfig {
   static final String websocketUrl = "wss://api.europeanschoolradio.eu/ws";
   static final String communityApiUrl = "https://community.europeanschoolradio.eu/wp-json/wp/v2";
   static final String communityBaseUrl = "https://community.europeanschoolradio.eu";
-  static final String webBaseUrl = "https://next-dev.europeanschoolradio.eu";
+  static final String webBaseUrl = "https://europeanschoolradio.eu";
 }
