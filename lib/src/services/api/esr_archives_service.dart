687f8fee818f130c60c1751ba6a2ad03cf569af7
George Papadopoulos <georgepap2001@gmail.com>
2025-08-05 13:00:26 +0300

    Added end time field to add archive request.

--- lib/src/services/api/esr_archives_service.dart
+++ lib/src/services/api/esr_archives_service.dart
@@ -29,6 +29,7 @@ class ESRArchivesService {
       "${archive.name.keys.first}[production]": archive.productionID.toString(),
       "${archive.name.keys.first}[broadcast_day]": ESRDateTimeFormatter.formatDateRequests(archive.broadcastDay),
       "${archive.name.keys.first}[start_time]": ESRDateTimeFormatter.formatTimeRequests(archive.startTime),
+      "${archive.name.keys.first}[end_time]": ESRDateTimeFormatter.formatTimeRequests(archive.endTime),
       "${archive.name.keys.first}[disabled]": archive.disabled ? "1": "0",
       "${archive.name.keys.first}[duration]": archive.audioFileDuration.toString(),
     });
