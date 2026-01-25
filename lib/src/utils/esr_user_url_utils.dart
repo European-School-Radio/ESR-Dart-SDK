97b04546dde8c81a7d4d63f70a9e3fed87326c1c
George Papadopoulos <georgepap2001@gmail.com>
2026-01-25 17:03:13 +0200

    Added static method to get user web url.

--- lib/src/utils/esr_user_url_utils.dart
+++ lib/src/utils/esr_user_url_utils.dart
@@ -1,7 +1,20 @@
+import 'package:esr_dart_sdk/esr_dart_sdk.dart';
+import 'package:esr_dart_sdk/src/enums/esr_environments.dart';
 import 'package:esr_dart_sdk/src/global_parameters/server_config.dart';
 
 class ESRUserUrlUtils {
+  static final sdk = ESRSDK();
+
   static String getBannerURL(int userID){
     return "${ESRServerConfig.communityBaseUrl}/wp-json/custom/get/user/banner/$userID";
+  }
+
+  static String getWebURL(int userID, {ESRLang? language}){
+    String lang = "en";
+    if (language != null){
+      lang = language.flag;
+    }
+
+    return "${sdk.env.value}/$lang/user/$userID";
   }
 }
