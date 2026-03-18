ef13baf0664ce01dfc4a332215704f5470da4f43
George Papadopoulos <georgepap2001@gmail.com>
2026-03-18 17:47:06 +0200

    Added get productions by user websocket class.

--- lib/src/services/api/websockets/esr_productions_websocket_service.dart
+++ lib/src/services/api/websockets/esr_productions_websocket_service.dart
@@ -1033,3 +1033,194 @@ class ESRProductionsBySchoolWebsocketService {
     }
   }
 }
+
+class ESRProductionsByUserWebsocketService {
+  final sdk = ESRSDK();
+  String _baseWebSocketURL = "";
+  ESRLang? _language;
+  int _pageSize = 1;
+  int _groupId = 1;
+  int? _userId;
+  ESRProductionSorting _sorting = ESRProductionSorting.created;
+  ESRSortingDirections _direction = ESRSortingDirections.desc;
+
+  bool _isConnected = false;
+  WebSocketChannel? _channel;
+  final StreamController<ESRProductionsWebsocketListResults> _controller =
+  StreamController<ESRProductionsWebsocketListResults>.broadcast();
+
+  ESRProductionsByUserWebsocketService() {
+    if (sdk.env == ESREnvironments.test) {
+      _baseWebSocketURL =
+      "${ESRServerConfig.websocketTestUrl}/productions-by-user/";
+    } else {
+      _baseWebSocketURL =
+      "${ESRServerConfig.websocketUrl}/productions-by-user/";
+    }
+  }
+
+  void setLanguage(ESRLang language) {
+    if (_isConnected) {
+      throw WebsocketAlreadyConnectedException("WebSocket is already connected");
+    }
+    _language = language;
+  }
+
+  ESRLang? getLanguage() {
+    return _language;
+  }
+
+  void setPageSize(int newMaxItems) {
+    _pageSize = newMaxItems;
+
+    if (_isConnected){
+      Map<String, String> message = {
+        "action": "paginate",
+        "page_size": _pageSize.toString()
+      };
+      String jsonMessage = jsonEncode(message);
+      _channel?.sink.add(jsonMessage);
+    }
+  }
+
+  int getPageSize() {
+    return _pageSize;
+  }
+
+  void setGroupId(int newGroupId){
+    _groupId = newGroupId;
+
+    if (_isConnected){
+      Map<String, String> message = {
+        "action": "paginate",
+        "group_id": _groupId.toString()
+      };
+      String jsonMessage = jsonEncode(message);
+      _channel?.sink.add(jsonMessage);
+    }
+  }
+
+  int getGroupId(){
+    return _groupId;
+  }
+
+  void setUserId(int newUserId){
+    if (_isConnected) {
+      throw WebsocketAlreadyConnectedException(
+          "WebSocket is already connected");
+    }
+
+    _userId = newUserId;
+  }
+
+  int? getUserId(){
+    return _userId;
+  }
+
+  void setSorting(ESRProductionSorting newSorting) {
+    _sorting = newSorting;
+
+    if (_isConnected){
+      Map<String, String> message = {
+        "action": "paginate",
+        "sort": _sorting.value.toString()
+      };
+      String jsonMessage = jsonEncode(message);
+      _channel?.sink.add(jsonMessage);
+    }
+  }
+
+  ESRProductionSorting getSorting(){
+    return _sorting;
+  }
+
+  void setDirection(ESRSortingDirections newDirection){
+    _direction = newDirection;
+
+    if (_isConnected){
+      Map<String, String> message = {
+        "action": "paginate",
+        "direction": _direction.value.toString()
+      };
+      String jsonMessage = jsonEncode(message);
+      _channel?.sink.add(jsonMessage);
+    }
+  }
+
+  ESRSortingDirections getDirection(){
+    return _direction;
+  }
+
+  void connect() {
+    if (_isConnected) {
+      throw WebsocketAlreadyConnectedException("WebSocket is already connected");
+    }
+
+    final urlBuilder = UrlBuilder(_baseWebSocketURL);
+    urlBuilder.addQueryParam("lang", (_language == null) ? "en" : _language!.flag);
+    urlBuilder.addQueryParam("page_size", _pageSize.toString());
+    urlBuilder.addQueryParam("sort", _sorting.value.toString());
+    urlBuilder.addQueryParam("direction", _direction.value.toString());
+    urlBuilder.addQueryParam("group_id", _groupId.toString());
+
+    if (_userId != null){
+      urlBuilder.addQueryParam("user_id", _userId.toString());
+    }
+
+    _channel = WebSocketChannel.connect(Uri.parse(urlBuilder.build()));
+    _isConnected = true;
+    _channel?.stream.listen(
+          (message) {
+        Map<String, dynamic> jsonMessage = jsonDecode(message);
+        _controller.add(ESRProductionsWebsocketListResults.fromJson(jsonMessage, _pageSize));
+      },
+      onError: (error) {
+        _isConnected = false;
+        disconnect();
+      },
+      onDone: () {
+        _isConnected = false;
+      },
+    );
+  }
+
+  Stream<ESRProductionsWebsocketListResults> get stream => _controller.stream;
+
+  StreamSubscription<ESRProductionsWebsocketListResults> addListener(
+      void Function(ESRProductionsWebsocketListResults event) onData,
+      {Function? onError,
+        void Function()? onDone,
+        bool? cancelOnError}) {
+    return _controller.stream.listen(
+      onData,
+      onError: onError,
+      onDone: onDone,
+      cancelOnError: cancelOnError,
+    );
+  }
+
+  void sendJson(Map<String, dynamic> message) {
+    if (!_isConnected) {
+      throw WebsocketNotConnectedException("WebSocket is NOT connected");
+    }
+
+    String jsonMessage = jsonEncode(message);
+
+    _channel?.sink.add(jsonMessage);
+  }
+
+  void disconnect() {
+    if (!_isConnected){
+      return;
+    }
+
+    _isConnected = false;
+
+    _channel?.sink.close(status.normalClosure);
+    _channel = null;
+
+    if (!_controller.isClosed) {
+      _controller.close();
+    }
+  }
+}
