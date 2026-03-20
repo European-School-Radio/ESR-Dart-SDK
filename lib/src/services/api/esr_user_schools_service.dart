e513f4f38196b9738ecb26c74268f0b6680c4845
George Papadopoulos <georgepap2001@gmail.com>
2026-03-20 17:24:43 +0200

    Added sorting, direction and language support to get active user schools for user method.

--- lib/src/services/api/esr_user_schools_service.dart
+++ lib/src/services/api/esr_user_schools_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:esr_dart_sdk/esr_dart_sdk.dart';
+import 'package:esr_dart_sdk/src/enums/directions/esr_sorting_directions.dart';
+import 'package:esr_dart_sdk/src/enums/sorting/esr_users_schools_sorting.dart';
 import 'package:esr_dart_sdk/src/global_parameters/server_config.dart';
 import 'package:esr_dart_sdk/src/utils/url_builder.dart';
 import 'package:http/http.dart' as http;
@@ -16,8 +18,25 @@ class ESRUserSchoolsService {
     }
   }
 
-  Future<ESRUserSchoolsActiveSchoolsResults> getActiveUserSchoolsForUser(int userID) async {
-    final urlBuilder = UrlBuilder('$_apiURL/user-school/byUser/$userID?page=1&limit=-1');
+  Future<ESRUserSchoolsActiveSchoolsResults> getActiveUserSchoolsForUser(int userID, {ESRUsersSchoolsSorting? sorting, ESRSortingDirections? direction, ESRLang? language}) async {
+    final urlBuilder = UrlBuilder('$_apiURL/user-school/byUser/$userID');
+
+    urlBuilder.addQueryParam("page", "1");
+    urlBuilder.addQueryParam("limit", "-1");
+
+    if (sorting != null){
+      urlBuilder.addQueryParam("sort", sorting.value.toString());
+    }
+
+    if (direction != null){
+      urlBuilder.addQueryParam("direction", direction.value.toString());
+    }
+
+    if (language != null){
+      urlBuilder.addQueryParam("lang", language.flag.toString());
+    } else {
+      urlBuilder.addQueryParam("lang", "en");
+    }
 
     var request = http.Request('GET', Uri.parse(urlBuilder.build()));
     http.StreamedResponse response = await request.send();
