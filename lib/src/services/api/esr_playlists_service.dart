c42daa64ea23a24a1d1a8669238ad518f423c204
George Papadopoulos <georgepap2001@gmail.com>
2026-03-22 00:31:30 +0200

    Added method to get playlist by id.

--- lib/src/services/api/esr_playlists_service.dart
+++ lib/src/services/api/esr_playlists_service.dart
@@ -18,6 +18,28 @@ class ESRPlaylistsService {
     }
   }
 
+  Future<ESRPlaylist> getPlaylistById(int id, {ESRLang? language}) async {
+    final urlBuilder = UrlBuilder('$_apiURL/playlist/$id');
+
+    if (language == null){
+      urlBuilder.addQueryParam("lang", "en");
+    } else {
+      urlBuilder.addQueryParam("lang", language.flag);
+    }
+
+    var request = http.Request('GET', Uri.parse(urlBuilder.build()));
+    http.StreamedResponse response = await request.send();
+    if (response.statusCode == 200) {
+      var responsePlain = await response.stream.bytesToString();
+      var jsonData = json.decode(responsePlain);
+      return ESRPlaylist.fromJson(jsonData['playlist']);
+    } else if (response.statusCode == 404){
+      throw ObjectNotFoundException("Playlist with id $id not found");
+    } else {
+      throw HttpRequestNotSucceededException(response.reasonPhrase ?? "HTTP Request not Succeeded");
+    }
+  }
+
   Future<ESRPlaylistsIncreaseSharesCounterResults> increaseSharesCount(int id) async {
     final urlBuilder = UrlBuilder('$_apiURL/playlist/share/$id');
 
