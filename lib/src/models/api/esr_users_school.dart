6dfa4b4897c65299a7165be48c024452e592730f
George Papadopoulos <georgepap2001@gmail.com>
2026-07-25 00:00:42 +0300

    Added numeric check on users schools model.

--- lib/src/models/api/esr_users_school.dart
+++ lib/src/models/api/esr_users_school.dart
@@ -1,10 +1,11 @@
 import 'package:esr_dart_sdk/esr_dart_sdk.dart';
+import 'package:esr_dart_sdk/src/utils/is_numeric.dart';
 
 class ESRUsersSchools {
   int id = 0;
-  ESRUser user;
-  ESRSchool school;
-  ESRRole role;
+  ESRUser? user;
+  ESRSchool? school;
+  ESRRole? role;
   bool disabled = false;
   DateTime created = DateTime.now();
   DateTime updated = DateTime.now();
@@ -22,9 +23,9 @@ class ESRUsersSchools {
   factory ESRUsersSchools.fromJson(Map<String, dynamic> json){
     return ESRUsersSchools(
         id: json['id'],
-        user: ESRUser.fromJson(json['user']),
-        school: ESRSchool.fromJson(json['school']),
-        role: ESRRole.fromJson(json['role']),
+        user: IsNumericUtils.isNumeric(json['user'].toString()) ? null : ESRUser.fromJson(json['user']),
+        school: IsNumericUtils.isNumeric(json['school'].toString()) ? null : ESRSchool.fromJson(json['school']),
+        role: IsNumericUtils.isNumeric(json['role'].toString()) ? null : ESRRole.fromJson(json['role']),
         disabled: json['disabled'],
         created: DateTime.parse(json['created']),
         updated: DateTime.parse(json['updated'])
